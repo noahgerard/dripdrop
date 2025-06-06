d4a3ed93a0a9b6fdcdbd3e7149e9639a21fb0564
noahgerard <helix.dev.xyz@gmail.com>
2025-06-06 14:41:49 -0600

    feat: Update forgot password page with funny message and comment out form

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,12 +1,14 @@
 <x-guest-layout>
     <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+        {{--         {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+ --}}
+        <marquee behavior="" direction="">{{ __('Forgot your password? Oh no...') }}</marquee>
     </div>
 
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="{{ route('password.email') }}">
+    {{-- <form method="POST" action="{{ route('password.email') }}">
         @csrf
 
         <!-- Email Address -->
@@ -21,5 +23,5 @@
                 {{ __('Email Password Reset Link') }}
             </x-primary-button>
         </div>
-    </form>
+    </form> --}}
 </x-guest-layout>
