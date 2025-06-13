9ea2869ef674e0c2480ce9a9a21da213dda7e682
Noah Gerard <noah.gerard@focus.org>
2025-06-13 10:21:41 -0600

    fix: Ensure Tailwind CSS is used for pagination in production environment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
 
@@ -23,5 +24,6 @@ class AppServiceProvider extends ServiceProvider
         if (app()->environment('production')) {
             URL::forceScheme('https');
         }
+        Paginator::useTailwind();
     }
 }
